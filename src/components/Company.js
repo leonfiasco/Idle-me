bab6520d582db32fa57b9656af7aabe72defa0d5
leon Boakye-Adjei <leon_adjei@hotmail.com>
2018-06-22 15:40:27 +0100

    finishing company banner

--- src/components/Company.js
+++ src/components/Company.js
@@ -10,11 +10,21 @@ class Company extends React.Component {
               <p className="txt text-white">Some businesses we've switched on<span className="dots1">...</span></p>
             </div>
               <div class="row-9">
-                <div className="circle"></div>
-                <div className="circle"></div>
-                <div className="circle"></div>
-                <div className="circle"></div>
-                <div className="circle"></div>
+                <div className="circle">
+                  <img className="comp-logo" src="https://dl.dropboxusercontent.com/content_link/2pAoeTySOlTtK9Jv0e1ahfMzFTENdTXMjyu1VxBw7WN9Tf0ofZwIAhgOkaezZFyW/file?dl=0&duc_id=P4vcQWBEyWstxBKytY5BQKvGpLCjI96pYeto07DOjW97JfqzNk4F8owe6uzyX7M9&raw=1&size=1280x960&size_mode=3"></img>
+                </div>
+                <div className="circle">
+                  <img className="comp-logo" src="https://dl.dropboxusercontent.com/content_link/W6aC0636OZTbkjYqOT14zHEi6weZmV4hwC4yEfvNyvAVLiRoiI0A266CfQ3K4G7H/file?dl=0&duc_id=DdEnUCQWe1Vf3iRXXyl3UoZ3k8qRSUuuY1iY7D7KEhAwo4fUp0H6nihDkT3le89d&raw=1&size=1280x960&size_mode=3"></img>
+                </div>
+                <div className="circle">
+                  <img className="comp-logo" src="https://dl.dropboxusercontent.com/content_link/oXDzk2599ITkBLUUrSkq7GnbCjG17Iq9JAKU8IO9t3PybpVM0LnReRxtCKrSAEHg/file?dl=0&duc_id=HSa3o4p6tz3kjIBjCUJnpcVI4WCm99dVHfABLTXx5vaOZgxzMKi2xE9u7wQGj5dF&raw=1&size=1280x960&size_mode=3"></img>
+                </div>
+                <div className="circle">
+                  <img className="comp-logo" src="https://dl.dropboxusercontent.com/content_link/BSosbinDaMc9A1GWptkZxx2X92pweYzK0DUeTVVWoSBXBeNUg7YzlwdAPJOMP87w/file?dl=0&duc_id=rCaX3VuXqrXOXX4Q6i683z0nCvikx0iFxbxoqVnSmkQNhAwuib5VyKKtmXQnQhnk&raw=1&size=1280x960&size_mode=3"></img>
+                </div>
+                <div className="circle">
+                  <img className="comp-logo" src="https://dl.dropboxusercontent.com/content_link/17SFSELFZqFaLXDsfWLzc4T4NSAbCHvPNBBcPv2G9IBnqhTbvdFUUgayWlL1QMzv/file?dl=0&duc_id=gS4h7hGxTghx1FuzJI7nrXsp6mHe8bH07GiLxn4TMBkQHRQ1WQ5S2beRawJefVZn&raw=1&size=1280x960&size_mode=3"></img>
+                </div>
               </div>
 
               <div class="row">

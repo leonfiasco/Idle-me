a560a1bf5acc82fcd5f0ea0c9a4f25803b232e72
leon Boakye-Adjei <leon_adjei@hotmail.com>
2018-08-23 16:15:02 +0100

    responsive company banner

--- src/components/Company.js
+++ src/components/Company.js
@@ -4,34 +4,18 @@ import React from 'react';
 class Company extends React.Component {
   render() {
     return (
-      <div className="box-model">
-        <div className="container">
-          <div className="row align-items-center">
-            <div className="row">
-              <p className="txt text-white" id="txt">Some businesses we've switched on<span className="dots1">...</span></p>
-            </div>
-              <div className="row-9">
-                <div className="circles">
-                  <img className="comp-logo1" id="comp-logo1" alt="virgin-media" src="/images/vm-Logo.png"></img>
-                </div>
-                <div className="circles">
-                  <img className="comp-logo2" id="comp-logo2" alt="four-corners" src="/images/four-corners-logo-2.png" href=""></img>
-                </div>
-                <div className="circles">
-                  <img className="comp-logo3" id="comp-logo3" alt="american-express" src="/images/American-Express-logo.png" href=""></img>
-                </div>
-                <div className="circles">
-                  <img className="comp-logo4" id="comp-logo4" alt="foundry" src="/images/the-foundry-logo.png" href=""></img>
-                </div>
-                <div className="circles">
-                  <img className="comp-logo5" id="comp-logo5" alt="TwinSmile" src="/images/TwinSmile-Logo-B2B.png" href=""></img>
-                </div>
-              </div>
-              <div className="row">
-              </div>
-          </div>
-          </div>
+      <div id="comp-banner">
+      <div class="row">
+          <p class="text-white" id="txt">Some businesses we've switched on<span className="dots1">...</span></p>
         </div>
+      <div id="container">
+          <img class="comp-logo1" alt="virgin-media" src="./images/vm-Logo.png"></img>
+            <img class="comp-logo2" alt="four-corners" src="./images/four-corners-logo-2.png" href=""></img>
+            <img class="comp-logo3" alt="american-express" src="./images/American-Express-logo.png" href=""></img>
+            <img class="comp-logo4" alt="foundry" src="./images/the-foundry-logo.png" href=""></img>
+          <img class="comp-logo5" alt="TwinSmile" src="./images/TwinSmile-Logo-B2B.png" href=""></img>
+      </div>
+    </div>
     );
   }
 }
